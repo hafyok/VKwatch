37788383e694ab9e20d84a3f369052d716523300
Haggai Khafizov <hafyok5777@gmail.com>
2024-03-16 19:27:26 +0300

    Initial commit

---
+++ app/src/main/java/com/example/vkwatch/AnalogClockView.kt
@@ -0,0 +1,13 @@
+package com.example.vkwatch
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+
+class AnalogClockView(
+    context: Context,
+    attributeSet: AttributeSet
+) : View(context, attributeSet){
+
+}
