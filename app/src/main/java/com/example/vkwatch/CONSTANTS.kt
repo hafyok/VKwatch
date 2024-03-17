466feee877058a99ec65803bf29f5309d4b2f84f
Haggai Khafizov <hafyok5777@gmail.com>
2024-03-17 12:26:10 +0300

    Relocate constants to CONSTANTS.kt

---
+++ app/src/main/java/com/example/vkwatch/CONSTANTS.kt
@@ -0,0 +1,25 @@
+package com.example.vkwatch
+
+import android.graphics.Color
+import android.graphics.Paint
+
+object CONSTANTS{
+    var mDialColor: Int = Color.GRAY
+    var mPointsColor: Int = Color.WHITE
+    var mHourHandColor: Int = Color.GREEN
+    var mMinuteHandColor: Int = Color.YELLOW
+    var mSecondHandColor: Int = Color.RED
+    var mHourHandWidth: Float = 15f
+    var mMinuteHandWidth: Float = 12f
+    var mSecondHandWidth: Float = 8f
+
+    // paints
+    var mTextPaint: Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    lateinit var mDialPaint: Paint
+    lateinit var mPointerPaint: Paint
+
+    // custom view or canvas width, height and clock radius
+    var mWidth = 0.0f
+    var mHeight = 0.0f
+    var mRadius = 0.0f
+}
